91d21d8f267e21117c2debce81e2b5d1ed47ca87
AnkitGourav <Ankitgourav035@gmail.com>
2020-05-23 09:14:37 +0530

    Utils: add method to get today's passed time
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
    Signed-off-by: Anush02198 <anush.4376@gmail.com>

--- core/java/com/android/internal/util/spiceos/SpiceOSUtils.java
+++ core/java/com/android/internal/util/spiceos/SpiceOSUtils.java
@@ -31,6 +31,8 @@ import android.os.Looper;
 import android.os.PowerManager;
 import android.os.SystemProperties;
 import android.os.SystemClock;
+import android.text.TextUtils;
+import android.text.format.Time;
 import android.view.InputDevice;
 import android.view.KeyCharacterMap;
 import android.view.KeyEvent;
@@ -49,6 +51,15 @@ public class SpiceOSUtils {
 
         NetworkInfo wifi = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
         return wifi.isConnected();
+    }
+
+	// Returns today's passed time in Millisecond
+    public static long getTodayMillis() {
+        final long passedMillis;
+        Time time = new Time();
+        time.set(System.currentTimeMillis());
+        passedMillis = ((time.hour * 60 * 60) + (time.minute * 60) + time.second) * 1000;
+        return passedMillis;
     }
 
 	// Check if device is connected to the internet
